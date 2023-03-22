9ea7392a1105328ec5bda4fee9b8676ee0113953
AndradeBH <andradehernadez5@gmail.com>
2023-03-22 17:45:36 -0600

    commit

--- administracion/src/app/components/users/users.component.ts
+++ administracion/src/app/components/users/users.component.ts
@@ -1,8 +1,9 @@
-import {AfterViewInit, Component, Inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit, ViewChild} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/services/user';
 import { UsersService } from 'src/app/services/users.service';
 import { DialogComfirmComponent } from '../dialog-comfirm/dialog-comfirm.component';
@@ -15,18 +16,65 @@ import { UserComponent } from '../user/user.component';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements AfterViewInit{
+export class UsersComponent implements AfterViewInit, OnInit{
   displayedColumns: string[] = ['id', 'name', 'nameUser', 'phone', 'email', 'option'];
   dataSource: any;
-  
+  user: any;
+  private isLoggedIn = new BehaviorSubject<boolean>(false);
+
+  gestionRoles:Boolean=false;
+  registrarAdmins:Boolean=false;
+  modificarDatosUser:Boolean=false;
+  usuarioInactivo:Boolean=false;
 
   constructor(
     private dataService:UsersService, 
     private router: Router,
-    private matDialog: MatDialog ) {     
+    private matDialog: MatDialog,
+    private serviceAuth:UsersService ) {     
+  }
+
+  ngOnInit(): void {
+    if(localStorage.getItem('user')!=null){
+            
+      const user:any = localStorage.getItem('user');    
+      const userObj = JSON.parse(user);      
+      const token = userObj.token;
+      const typeUser = userObj.typeUser;
+
+      this.serviceAuth.getRolPrivUser({id_rol:typeUser, id_Privilegio:13}).subscribe(res=>{            
+        let arr = Object.entries(res);
+        if(arr[0][0] == "success"){
+          this.gestionRoles=true
+        }
+      })     
+
+      this.serviceAuth.getRolPrivUser({id_rol:typeUser, id_Privilegio:3}).subscribe(res=>{            
+        let arr = Object.entries(res);
+        if(arr[0][0] == "success"){
+          this.registrarAdmins=true
+        }
+      })     
+
+      this.serviceAuth.getRolPrivUser({id_rol:typeUser, id_Privilegio:4}).subscribe(res=>{            
+        let arr = Object.entries(res);
+        if(arr[0][0] == "success"){
+          this.modificarDatosUser=true
+        }
+      })  
+      
+      this.serviceAuth.getRolPrivUser({id_rol:typeUser, id_Privilegio:6}).subscribe(res=>{            
+        let arr = Object.entries(res);
+        if(arr[0][0] == "success"){
+          this.usuarioInactivo=true
+        }
+      })  
+        
+
+    }
   }
   
-  user: any;
+  
 
 
   @ViewChild(MatPaginator)
@@ -35,6 +83,27 @@ export class UsersComponent implements AfterViewInit{
   ngAfterViewInit() {    
     this.getUsersData();
   }
+
+  status(){
+    const localData :any = localStorage.getItem('user');
+    if(!localData){     
+      this.isLoggedIn.next(false);
+      console.log('User not logged in !!')
+      
+    }else{      
+      const userObj = JSON.parse(localData);
+      const token_expires_at = new Date(userObj.token_expies_at);
+      const current_date = new Date();
+      
+      if(token_expires_at > current_date){
+        this.isLoggedIn.next(true)
+      }else{
+        this.isLoggedIn.next(false);
+        console.log('token expires !!')
+      }
+    }
+  }
+
   getUsersData(){
     this.dataService.getData().subscribe(res => {
       this.user=res;
